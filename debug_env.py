358931067ad93823c0c2bcbffa864d632c534f09
AMMar1n <amonroymarin@gmail.com>
2026-04-09 19:21:23 -0600

    Debug variables Railway

---
+++ debug_env.py
@@ -0,0 +1,4 @@
+import os
+print("BINANCE_API_KEY:", os.getenv("BINANCE_API_KEY", "NO ENCONTRADA"))
+print("BINANCE_API_SECRET:", os.getenv("BINANCE_API_SECRET", "NO ENCONTRADA"))
+print("CMC_API_KEY:", os.getenv("CMC_API_KEY", "NO ENCONTRADA"))
